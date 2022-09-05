65bb6ac868e2bb796da937c4af7ab413b86dab86
Ray <105173457+RayIssam@users.noreply.github.com>
2022-09-05 18:42:00 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,38 +1,24 @@
-//fetch videos from youtube API
-const url="https://youtube.googleapis.com/youtube/v3/search?part=snippet&channelId=UC98Suvqo2atFD0Y6e3Bn2AA&maxResults=3&order=date&key=AIzaSyBOnawFtJGkZJhFMHaE3hb_rqrN9QwSsG0"
-fetch(url)
-.then(response => {return response.json() })
-.then(data =>{youtubeLatestVideos(data.items);})
-
-function youtubeLatestVideos(data){
-    //console.log(data)
-
-data.forEach(element => {
-  console.log(data)
-    let title=element.snippet.title;
-    let image=element.snippet.thumbnails.high.url;
-    let videoId=element.id.videoId;
-    let icon =document.createElement("span").innerHTML='<i class="fa-solid fa-play icon"></i>'
-
-    /*   
-    icon.addeventlistener("click",loadVideo)
-    function loadVideo(){
-      document.getElementById("latest-videos").innerHTML +=
-      `<div> <iframe src="https://www.youtube.com/embed/${videoId}"></iframe></div>`
+    async function get(){
+      try{
+          const response = await fetch("https://youtube.googleapis.com/youtube/v3/search?part=snippet&channelId=UC98Suvqo2atFD0Y6e3Bn2AA&maxResults=3&order=date&key=AIzaSyBIwzMzUzXLoxKEiH-Y7qFZI1f0Jbea4so");
+          const data = await response.json();
+          console.log(data)
+              for(let i=0;i<3;i++){
+                let img=data.items[i].snippet.thumbnails.high.url
+                let title=data.items[i].snippet.title;
+                let videoId=data.items[i].id.videoId;
+                let button=document.getElementsByClassName("clickme")[i];
+                document.getElementsByClassName("img")[i].src =img;
+                document.getElementsByClassName("Vtitle")[i].innerHTML=title;
+                button.addEventListener("click",function(){
+                  let box =document.getElementsByClassName("box")[i]
+                  box.innerHTML=`<div> <iframe src="https://www.youtube.com/embed/${videoId}?autoplay=1" allow="autoplay"  width="300px" height="200" ></iframe></div>`
+                })
+              }
+      }
+      catch(error){
+        
+        console.log(error);
     }
-    
-      */
-    let latestVideos = document.getElementById("latest-videos").innerHTML +=
-    `
-    <div class="box"> 
-    ${icon} 
-    <img class="thumbnail" src="${image}" width="300px"/>
-    <p class="title">${title}</p>
-  </div>
-    `
-   
-});
-
-
-}
-
+    }
+    get();
